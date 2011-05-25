2c1fd5474f3e12ae7851fad9c4c91316c1c65828
Cory Radel <cory.radel@gmail.com>
2011-05-25 12:02:19 +0200

    Initial.

---
+++ My Documents/Aptana Studio 3 Workspace/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '18ab1f6e1b6c27a956a80bedfac5fac33513bdc66567ac3d0736fb4c953151d2ad6853838e66707c4749484b9ea6b687f58a4d7ca9698be045571c986b148b9a'
